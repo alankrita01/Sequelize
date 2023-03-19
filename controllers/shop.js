81761f6f351f0b2d4b69d317e767f5989403f8cc
Alankrita Patidar <2102561>
2023-03-19 15:25:10 +0530

    updating shop.js controller file

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,36 +54,72 @@ exports.getIndex = (req, res, next) => {                         //findAll to ge
 
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
+ //console.log(req.user.cart);
+req.user
+.getCart()
+.then(cart => {
+  //console.log(cart)
+  return cart.
+  getProducts()
+  .then(products => {
+    res.render('shop/cart', {
+      path: '/cart',
+      pageTitle: 'Your Cart',
+      products: products
     });
-  });
+  })
+  .catch(err => console.log(err));
+})
+.catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchCart;
+  let newQantity = 1;
+  req.user
+  .getCart()
+  .then(cart => {
+    fetchCart = cart;
+    return cart.getProducts({where: {id:prodId}});
+  })
+  .then(products => {
+    let product;
+    if(products.length>0){
+      product = products[0];
+    }
+    
+    if(product){
+      const oldQuantity = product.cartItem.quantity;
+      newQantity = oldQuantity +1;
+     return product;
+    }
+    return Product.findById(prodId)
+  })
+  .then(data => {
+    return fetchCart.addProduct(product,{through: {quantity: newQantity}})
+  })
+  .then(() => {
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart.getProduct({where: {id: prodId}})
+  })
+  .then(products => {
+    const product = products[0];
+    product.cartItem.destroy();
+  })
+  .then(result => {
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
   Product.findById(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
